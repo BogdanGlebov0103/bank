70ce9f39464a551e92c248c5d73eebb0d629767d
Bogdan <Bogdan0103@ya.ru>
2026-08-05 14:09:14 +0300

    добавил аннотацию типов

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ list_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.
   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-def filter_by_state(list_dict, state='EXECUTED'):
+def filter_by_state(list_dict: list, state='EXECUTED') -> list:
     '''
     возвращает новый список, где значение совпадает со значением, указанным в параметре
     '''
@@ -13,7 +13,7 @@ def filter_by_state(list_dict, state='EXECUTED'):
     return new_list_dict
 
 
-def sort_by_date(list_dict, decreasing = True):
+def sort_by_date(list_dict: list, decreasing = True) -> list:
     '''
     фильтрует по дате на убывание
     '''
