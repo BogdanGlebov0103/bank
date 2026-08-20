5886749e6bdf9ac60e93763192fe75198ee46cdc
Bogdan <Bogdan0103@ya.ru>
2026-08-20 14:04:37 +0300

    исправил замечания

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,18 @@
-# 7000 7922 89606361     # входной аргумент
-# 7000 79** **** 6361  # выход функции
-number = "1234567891234567"
-
-
 def get_mask_card_number(number: str) -> str:
+    '''
+    скрывает некоторые цифры номера карты
+    '''
     return f"{number[0:4]} {number[4:6]}** **** {number[12:17]}"
 
 
 def get_mask_account(number: str) -> str:
+    '''
+    маскирует номер счета
+    '''
     return "**" + number[-4:]
 
+
 if __name__ == '__main__':
+    number = "1234567891234567"
     print(get_mask_card_number(number))
     print(get_mask_account(number))
--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,3 @@
-list_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
- {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
 def filter_by_state(list_dict: list, state='EXECUTED') -> list:
     '''
     возвращает новый список, где значение совпадает со значением, указанным в параметре
@@ -20,5 +15,10 @@ def sort_by_date(list_dict: list, decreasing = True) -> list:
     return sorted(list_dict, key=lambda d: d['date'], reverse=decreasing)
 
 
-f_s = print(filter_by_state(list_dict, state='EXECUTED'))
-s_d = print(sort_by_date(list_dict, decreasing = True))
+if __name__ == '__main__':
+    list_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    print(filter_by_state(list_dict, state='EXECUTED'))
+    print(sort_by_date(list_dict, decreasing = True))
